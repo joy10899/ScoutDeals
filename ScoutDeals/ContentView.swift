6fedd57dc3b3529c721e271302cda64cbb9824e3
joytran10899 <Chocolate2023>
2026-06-27 15:40:38 -0700

    Initial Commit

---
+++ ScoutDeals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoutDeals
+//
+//  Created by Joy on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoutDeals/ScoutDealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutDealsApp.swift
+//  ScoutDeals
+//
+//  Created by Joy on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoutDealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
